bb905cb82c42e365e63434649e96caa1544acc58
Surabhi Mahale <surabhimahale12@gmail.com>
2023-06-04 15:41:57 +0530

    Router, Header and footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
-import About from './components/About'
-import Contact from './components/Contact'
-import Services from './components/Services'
-import Home from './components/Home'
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Services from "./components/Services";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./components/Home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 export const App = () => {
   return (
-    <div>This is Multipage website</div>
-  )
-}
+    <>
+      <BrowserRouter>
+      <Header/>
+        <Routes>
+          <Route path="/" element= {<Home/>} />
+          <Route path="/about" element= {<About/>} />
+          <Route path="/services" element= {<Services/>} />
+          <Route path="/contact" element= {<Contact/>} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App
