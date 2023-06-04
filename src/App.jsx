f959913e3fea9eb2a9d53fbf352f85d4d5067052
Surabhi Mahale <surabhimahale12@gmail.com>
2023-06-04 15:08:25 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import About from './components/About'
+import Contact from './components/Contact'
+import Services from './components/Services'
+import Home from './components/Home'
+export const App = () => {
+  return (
+    <div>This is Multipage website</div>
+  )
+}
